3402dce7912a9778c0790295a155e16445fc373c
Jizu Sun <jizu.sun@sap.com>
2020-08-04 19:21:14 +0800

    save

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': ['warn', 'always'],
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
